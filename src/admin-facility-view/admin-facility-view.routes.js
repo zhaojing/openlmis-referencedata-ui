ff0807c1318bf2c2af4de17aac2961e9d10df4f2
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-04-25 12:09:20 +0200

    OLMIS-2284: added view facility screen

---
+++ src/admin-facility-view/admin-facility-view.routes.js
@@ -0,0 +1,44 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+	'use strict';
+
+	angular.module('admin-facility-view').config(routes);
+
+	routes.$inject = ['$stateProvider', 'ADMINISTRATION_RIGHTS'];
+
+	function routes($stateProvider, ADMINISTRATION_RIGHTS) {
+
+		$stateProvider.state('openlmis.administration.facilities.view', {
+			label: 'adminFacilityView.viewFacility',
+			url: '/facilities/:id',
+			accessRights: [ADMINISTRATION_RIGHTS.FACILITIES_MANAGE],
+            views: {
+                '@openlmis': {
+                    controller: 'FacilityViewController',
+                    templateUrl: 'admin-facility-view/facility-view.html',
+                    controllerAs: 'vm',
+                }
+            },
+			resolve: {
+				facility: function(facilityService, $stateParams) {
+					return facilityService.get($stateParams.id);
+				}
+			}
+		});
+	}
+})();
---
+++ src/admin-facility-view/facility-view.controller.js
@@ -0,0 +1,78 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+	'use strict';
+
+    /**
+     * @ngdoc controller
+     * @name admin-facility-view.controller:FacilityViewController
+     *
+     * @description
+     * Controller for managing facility view screen.
+     */
+	angular
+		.module('admin-facility-view')
+		.controller('FacilityViewController', controller);
+
+	controller.$inject = ['$state', 'facility'];
+
+	function controller($state, facility) {
+
+		var vm = this;
+
+        vm.$onInit = onInit;
+        vm.goToFacilityList = goToFacilityList;
+
+        /**
+         * @ngdoc property
+         * @propertyOf admin-facility-view.controller:FacilityViewController
+         * @name facility
+         * @type {Object}
+         *
+         * @description
+         * Contains facility object.
+         */
+        vm.facility = undefined;
+
+        /**
+         * @ngdoc method
+         * @propertyOf admin-facility-view.controller:FacilityViewController
+         * @name $onInit
+         *
+         * @description
+         * Method that is executed on initiating FacilityListController.
+         */
+        function onInit() {
+			vm.facility = facility;
+        }
+
+        /**
+         * @ngdoc method
+         * @methodOf admin-facility-list.controller:FacilityListController
+         * @name goToFacilityList
+         *
+         * @description
+         * Redirects to facility list screen.
+         */
+		function goToFacilityList() {
+            $state.go('^', {}, {
+                reload: true
+            });
+		}
+	}
+
+})();
