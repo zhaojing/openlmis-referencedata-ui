17097ddadabb91eaf2d86f7c72c593cce9ad8b04
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2017-05-16 15:24:50 +0200

    OLMIS-2492: changed dropdown on role assignment screen to use only warehouses

--- src/admin-user-roles/admin-user-roles.routes.js
+++ src/admin-user-roles/admin-user-roles.routes.js
@@ -15,49 +15,57 @@
 
 (function() {
 
-	'use strict';
+    'use strict';
 
-	angular.module('admin-user-roles').config(routes);
+    angular.module('admin-user-roles').config(routes);
 
-	routes.$inject = ['$stateProvider', 'ADMINISTRATION_RIGHTS'];
+    routes.$inject = ['$stateProvider', 'ADMINISTRATION_RIGHTS'];
 
-	function routes($stateProvider, ADMINISTRATION_RIGHTS) {
+    function routes($stateProvider, ADMINISTRATION_RIGHTS) {
 
-		$stateProvider.state('openlmis.administration.users.roles', {
-			label: 'adminUserRoles.editUserRoles',
-			url: '/users/:id/roles',
-			accessRights: [ADMINISTRATION_RIGHTS.USERS_MANAGE],
-			resolve: {
-				roles: function(referencedataRoleFactory) {
-					return referencedataRoleFactory.getAllWithType();
-				},
-				programs: function(programService) {
-					return programService.getAll();
-				},
-				supervisoryNodes: function(supervisoryNodeFactory) {
-					return supervisoryNodeFactory.getAllSupervisoryNodesWithDisplay();
-				},
-				warehouses: function(facilityService) {
-					return facilityService.getAll();
-				},
-				user: function($q, referencedataUserService, userRoleAssignmentFactory, $stateParams, roles, programs, supervisoryNodes, warehouses) {
-					var deferred = $q.defer();
+        $stateProvider.state('openlmis.administration.users.roles', {
+            label: 'adminUserRoles.editUserRoles',
+            url: '/users/:id/roles',
+            accessRights: [ADMINISTRATION_RIGHTS.USERS_MANAGE],
+            resolve: {
+                roles: function(referencedataRoleFactory) {
+                    return referencedataRoleFactory.getAllWithType();
+                },
+                programs: function(programService) {
+                    return programService.getAll();
+                },
+                supervisoryNodes: function(supervisoryNodeFactory) {
+                    return supervisoryNodeFactory.getAllSupervisoryNodesWithDisplay();
+                },
+                warehouses: function($q, facilityService) {
+                    var deferred = $q.defer();
 
-					referencedataUserService.get($stateParams.id).then(function(user) {
-						userRoleAssignmentFactory.addInfoToRoleAssignments(user.roleAssignments, roles, programs, supervisoryNodes, warehouses);
-						deferred.resolve(user);
-					}, deferred.reject);
+                    facilityService.search(null, {
+                        type: 'warehouse'
+                    }).then(function(response) {
+                        deferred.resolve(response.content);
+                    }, deferred.reject);
 
-					return deferred.promise;
-				}
-			},
-			views: {
+                    return deferred.promise;
+                },
+                user: function($q, referencedataUserService, userRoleAssignmentFactory, $stateParams, roles, programs, supervisoryNodes, warehouses) {
+                    var deferred = $q.defer();
+
+                    referencedataUserService.get($stateParams.id).then(function(user) {
+                        userRoleAssignmentFactory.addInfoToRoleAssignments(user.roleAssignments, roles, programs, supervisoryNodes, warehouses);
+                        deferred.resolve(user);
+                    }, deferred.reject);
+
+                    return deferred.promise;
+                }
+            },
+            views: {
                 '@openlmis': {
                     controller: 'UserRolesController',
                     templateUrl: 'admin-user-roles/user-roles.html',
                     controllerAs: 'vm',
                 }
             }
-		});
-	}
+        });
+    }
 })();
