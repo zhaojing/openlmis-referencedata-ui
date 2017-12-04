b4ed03a75965e2fc856f5657ce1637f51fd8da95
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-12-04 14:14:19 +0100

    OLMIS-3295: Added lot module

---
+++ src/referencedata-lot/lot-data-builder.spec.js
@@ -0,0 +1,55 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    angular
+        .module('referencedata-lot')
+        .factory('LotDataBuilder', LotDataBuilder);
+
+    LotDataBuilder.$inject = ['Lot'];
+
+    function LotDataBuilder(Orderable) {
+
+        LotDataBuilder.prototype.build = build;
+
+        return LotDataBuilder;
+
+        function LotDataBuilder() {
+            LotDataBuilder.instanceNumber = (LotDataBuilder.instanceNumber || 0) + 1;
+
+            this.id = 'lot-id-' + LotDataBuilder.instanceNumber;
+            this.lotCode = 'L' + LotDataBuilder.instanceNumber;
+            this.expirationDate = '2017-05-02T05:59:51.993Z';
+            this.manufactureDate = null;
+            this.tradeItem = null;
+        }
+
+        function build() {
+            return new Lot(
+                this.id,
+                this.lotCode,
+                this.expirationDate,
+                this.manufactureDate,
+                this.tradeItem,
+                true
+            );
+        }
+
+    }
+
+})();
---
+++ src/referencedata-lot/lot.js
@@ -0,0 +1,62 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2017 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details. You should have received a copy of
+ * the GNU Affero General Public License along with this program. If not, see
+ * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+(function() {
+
+    'use strict';
+
+    /**
+     * @ngdoc service
+     * @name referencedata-lot.Lot
+     *
+     * @description
+     * Represents a single lot.
+     */
+    angular
+        .module('referencedata-lot')
+        .factory('Lot', Lot);
+
+    function Lot() {
+
+        return Lot;
+
+        /**
+         * @ngdoc method
+         * @methodOf referencedata-lot.Lot
+         * @name Lot
+         *
+         * @description
+         * Creates a new instance of the Lot class.
+         *
+         * @param  {String}  id                 the id
+         * @param  {String}  lotCode            the lot code
+         * @param  {String}  expirationDate     the expiration date
+         * @param  {String}  manufactureDate    the manufacture date
+         * @param  {Object}  tradeItem          the trade item
+         * @param  {Boolean}  active            true if active; otherwise false
+         * @return {Lot}                        the lot object
+         */
+        function Lot(id, lotCode, expirationDate, manufactureDate, tradeItem, active) {
+            this.id = id;
+            this.lotCode = lotCode;
+            this.expirationDate = expirationDate;
+            this.manufactureDate = manufactureDate;
+            this.tradeItem = tradeItem;
+            this.active = active;
+        }
+
+    }
+
+})();
