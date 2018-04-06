0eca58b4339e182043b631519be377f2f660a490
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-04-06 14:34:57 +0200

    OLMIS-4515: Added unit tests for OrderableFulfillsResource class

--- src/referencedata-orderable-fulfills/orderable-fulfills-resource.spec.js
+++ src/referencedata-orderable-fulfills/orderable-fulfills-resource.spec.js
@@ -15,14 +15,21 @@
 
 describe('OrderableFulfillsResource', function() {
 
-    var OrderableFulfillsResource, OpenlmisResourceMock, referencedataUrlFactory;
+    var OrderableFulfillsResource, OpenlmisResourceMock, referencedataUrlFactory, parameterSplitterMock;
 
-    beforeEach(function() {
+    it('should extend OpenlmisResource', function() {
         module('referencedata-orderable-fulfills', function($provide) {
             OpenlmisResourceMock = jasmine.createSpy('OpenlmisResource');
 
             $provide.factory('OpenlmisResource', function() {
                 return OpenlmisResourceMock;
+            });
+
+            $provide.factory('ParameterSplitter', function() {
+                return function() {
+                    parameterSplitterMock = jasmine.createSpyObj('ParameterSplitter', ['split']);
+                    return parameterSplitterMock;
+                };
             });
         });
 
@@ -30,13 +37,120 @@ describe('OrderableFulfillsResource', function() {
             referencedataUrlFactory = $injector.get('referencedataUrlFactory');
             OrderableFulfillsResource = $injector.get('OrderableFulfillsResource');
         });
-    });
 
-    it('should extend OpenlmisResource', function() {
         new OrderableFulfillsResource();
 
         expect(OpenlmisResourceMock).toHaveBeenCalledWith(
             referencedataUrlFactory('/api/orderableFulfills')
         );
+    });
+
+    ddescribe('query', function() {
+
+        var params, responseOne, responseTwo, orderableFulfillsResource, $httpBackend, openlmisUrlFactory;
+
+        beforeEach(function() {
+            module('referencedata-orderable-fulfills', function($provide) {
+                $provide.factory('ParameterSplitter', function() {
+                    return function() {
+                        parameterSplitterMock = jasmine.createSpyObj('ParameterSplitter', ['split']);
+                        return parameterSplitterMock;
+                    };
+                });
+            });
+
+            inject(function($injector) {
+                referencedataUrlFactory = $injector.get('referencedataUrlFactory');
+                OrderableFulfillsResource = $injector.get('OrderableFulfillsResource');
+                $httpBackend = $injector.get('$httpBackend');
+                openlmisUrlFactory = $injector.get('openlmisUrlFactory');
+            });
+
+            orderableFulfillsResource = new OrderableFulfillsResource();
+
+            params = {
+                some: [
+                    'paramOne',
+                    'paramTwo'
+                ]
+            };
+
+            parameterSplitterMock.split.andReturn([{
+                some: ['paramOne']
+            }, {
+                some: ['paramTwo']
+            }]);
+
+            responseOne = {
+                idOne: ['idTwo', 'idThree']
+            };
+
+            responseTwo = {
+                idFour: ['ifFive', 'idSix']
+            };
+        });
+
+        it('should return response if only one request was sent', function() {
+            var params = {
+                some: 'param'
+            };
+
+            parameterSplitterMock.split.andReturn([params]);
+
+            $httpBackend
+                .expectGET(openlmisUrlFactory('/api/orderableFulfills?some=param'))
+                .respond(200, responseOne);
+
+            var result;
+            orderableFulfillsResource.query(params)
+            .then(function(response) {
+                result = response;
+            });
+            $httpBackend.flush();
+
+            expect(angular.toJson(result)).toEqual(angular.toJson(responseOne));
+        });
+
+        it('should reject if any of the requests fails', function() {
+            $httpBackend
+                .expectGET(openlmisUrlFactory('/api/orderableFulfills?some=paramOne'))
+                .respond(200, responseOne);
+
+            $httpBackend
+                .expectGET(openlmisUrlFactory('/api/orderableFulfills?some=paramTwo'))
+                .respond(500);
+
+            var rejected;
+            orderableFulfillsResource.query(params)
+            .catch(function() {
+                rejected = true;
+            });
+            $httpBackend.flush();
+
+            expect(rejected).toEqual(true);
+        });
+
+        iit('should return merged responses if multiple requests were sent', function() {
+            $httpBackend
+                .expectGET(openlmisUrlFactory('/api/orderableFulfills?some=paramOne'))
+                .respond(200, responseOne);
+
+            $httpBackend
+                .expectGET(openlmisUrlFactory('/api/orderableFulfills?some=paramTwo'))
+                .respond(200, responseTwo);
+
+            var result;
+            orderableFulfillsResource.query(params)
+            .then(function(response) {
+                result = response;
+            });
+            $httpBackend.flush();
+
+            expect(angular.toJson(result)).toEqual(angular.toJson({
+                idOne: ['idTwo', 'idThree'],
+                idFour: ['ifFive', 'idSix']
+            }));
+        });
+
     });
 });
