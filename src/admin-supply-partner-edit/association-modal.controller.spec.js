e0aa40a61cf1e194bffdcfb2076c16b7913294be
Nikodem Graczewski <ngraczewski@soldevelo.com>
2018-11-29 16:16:11 +0100

    OLMIS-5356: Removed ddescribe

--- src/admin-supply-partner-edit/association-modal.controller.spec.js
+++ src/admin-supply-partner-edit/association-modal.controller.spec.js
@@ -13,7 +13,7 @@
  * http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-ddescribe('AssociationModalController', function() {
+describe('AssociationModalController', function() {
 
     beforeEach(function() {
         module('admin-supply-partner-edit');
